e7fa796f0eb1b34d224c2c577d1179c488bed051
Erik Jenner <jupiter24@users.noreply.github.com>
2014-11-04 18:32:20 +0100

    Added config.php

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php
+	/* Database connection details */
+	define('HOST', 'localhost');
+	define('USER', 'webchess');
+	define('DATABASE', 'webchess');
+	define('PASSWORD', 'rPaAqPfUbuj3RRU9');
+?>
