36260f421df5d55e08863bbbb45f8de37ff196e4
Erik Jenner <jupiter24@users.noreply.github.com>
2014-11-05 16:13:38 +0100

    Add webchess.sql

--- include/user_management.php
+++ include/user_management.php
@@ -1,11 +1,12 @@
 <?php
+	include_once 'SecureSessionHandler.php'
 	
 	/********************
 	 * function login
 	 * returns true if the username-password combination is correct and sets $_SESSION values
 	 *******************/
 	
-	function login($username, $password, $PDOHandle) {
+	function login($username, $password, $PDOHandle, $currentSession) {
 		$stm = $PDOHANDLE->prepare('SELECT id, username, password, salt FROM users WHERE username = :username LIMIT 1;');
 		$stm->bindParam(':username', $username);
 		$stm->execute();
@@ -14,8 +15,8 @@
 		
 		$password = hash('sha512', $password . $result['salt']);
 		if ($password == $result['password']) {
-			$_SESSION['user_id'] = $result['id'];
-			$_SESSION['username'] = $result['username'];
+			$currentSession->put('user.id', $result['id']);
+			$currentSession->put('user.name', $result['username']);
 		}
 	}
 ?>
