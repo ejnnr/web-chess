a90ba2e0b7483a99e5ab51eb212fc0d6bb8f71ee
Erik Jenner <jupiter24@users.noreply.github.com>
2014-11-06 18:27:56 +0100

    Added protection against hijacking and fixed some bugs

--- include/user_management.php
+++ include/user_management.php
@@ -7,16 +7,54 @@
 	 *******************/
 	
 	function login($username, $password, $PDOHandle, $currentSession) {
-		$stm = $PDOHANDLE->prepare('SELECT id, username, password, salt FROM users WHERE username = :username LIMIT 1;');
+		// query user information from database
+		$stm = $PDOHandle->prepare('SELECT id, username, password, salt FROM users WHERE username = :username LIMIT 1;');
 		$stm->bindParam(':username', $username);
 		$stm->execute();
-		$stm->setFetchMode(PDO::FETCH_ASSOC);
-		$result = $stm->fetch();
+		$result = $stm->fetchAll();
 		
-		$password = hash('sha512', $password . $result['salt']);
-		if ($password == $result['password']) {
-			$currentSession->put('user.id', $result['id']);
-			$currentSession->put('user.name', $result['username']);
+		// check if the user exists
+		if (count($result) == 0) {
+			return false;
+		}
+		
+		// create the hash of password and salt
+		$password = hash('sha512', $password . $result[0]['salt']);
+		
+		if ($password == $result[0]['password']) {
+			
+			// set user.sessionString (used as some kind of protections against hijacking)
+			$currentSession->put('user.sessionString', hash('sha512', $password . $_SERVER['HTTP_USER_AGENT']));
+			
+			// set user.id and user.name
+			$currentSession->put('user.id', $result[0]['id']);
+			$currentSession->put('user.name', $result[0]['username']);
+			
+			return true;
 		}
 	}
+	
+	/********************
+	 * function isLoggedIn
+	 * returns true if some user is logged in
+	 *******************/
+	 
+	 function isLoggedIn($PDOHandle, $currentSession) {
+		 if (!empty($currentSession->get('user.id'), $currentSession->get('user.name'), $currentSession->get('user.sessionString'))) {
+			// query password hash from database
+			$stm = $PDOHandle->prepare('SELECT password FROM users WHERE id = :id LIMIT 1;');
+			$stm->bindParam(':id', $currentSession->get('user.id'));
+			$stm->execute();
+			$result = $stm->fetchAll();
+			
+			// check if the user exists
+			if (count($result) == 0) {
+				return false;
+			}
+			
+			if (hash('sha512', $result[0]['password'] . $_SERVER['HTTP_USER_AGENT']) == $currentSession->get('user.sessionString')) {
+				return true;
+			}
+		 }
+	 }
 ?>
