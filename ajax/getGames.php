200ac9ddb429a9e06f356d6a2a882293d3074750
Erik Jenner <jupiter24@users.noreply.github.com>
2015-02-26 16:11:03 +0000

    Improved ajax placeholders

--- ajax/getGames.php
+++ ajax/getGames.php
@@ -5,11 +5,12 @@
 	 * whether the user is logged in. */
 	
 	/* Syntax:
-	 * database name, number of games, owner's user name, 
+	 * game id, site, event, round, date,
+	 * number of moves, white player, black player, result,
+	 * white rating, black rating, ECO code,
 	 * last changed (timestamp), creation date (timestamp), 
-	 * visibility (private/shared/public), 
 	 * tags(may be empty, single tags are seperated by spaces); 
-	 * <next database> */
+	 * <next game> */
 	 
-	echo "My games|67|chessexpert77|1424610296|1424603719|shared|annotated favorite;Master games|1422|chessexpert77|1424603406|1424603406|public|";
+	echo "1|Wjik aan Zee|Tata Steel|3|43|Morphy, P.|Kasparov, G.|0|3462|5466|A00|1424610296|1424603719|not-a-real-game";
 ?>
