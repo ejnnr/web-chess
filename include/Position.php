9d26d38d87e99e645de077c0cd1454347295be89
Erik Jenner <jupiter24@users.noreply.github.com>
2015-03-15 12:41:46 +0100

    Add Position.php

---
+++ include/Position.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file contains the class Position and the corresponding exception class.
+ */
+
+/**
+ * Represents an exception thrown by Position.
+ * List of Exception Codes:
+ *   1: Unknown internal error
+ *   2: Null argument
+ *   3: Wrong argument count
+ *   4: Wrong argument type
+ *   5: Invalid argument value (general)
+ *   6: Invalid argument syntax (strings)
+ *   7: Numerical argument outside of allowed range
+ * 101: Trying to load impossible position (general)
+ * 102: Impossible position: one or two kings are missing
+ * 103: Impossible position: too many pieces of one kind
+ * 104: Impossible position: side not to move is in check
+ * 104: Impossible position: both sides are in check
+ * 105: Impossible position: pawns on 1st or 8th rank
+ * 110: Invalid FEN syntax
+ */
+
+?>
