a4861d658ff846f2ba4596045aa79c181ca5dcf9
jupiter24 <jupiter24@users.noreply.github.com>
2015-05-03 18:04:41 +0200

    Add features to GameNode
    GameNode was just a copy of Tree\Node\Node so far but now it
     - only allows moves as values
     - has a function called getMainline()

--- include/GameNode.php
+++ include/GameNode.php
@@ -1,5 +1,7 @@
 <?php
 
+class GameNodeException extends Exception {}
+
 /**
  * A class representing a node (i.e. a move) in a game of chess
  *
@@ -8,5 +10,29 @@
 
 class GameNode implements Tree\Node\NodeInterface
 {
-	use Tree\Node\NodeTrait;
+	use Tree\Node\NodeTrait
+   	{
+		__construct as traitConstruct;
+	}
+
+	public function __construct($move, array $children = [])
+	{
+		if (!($move instanceof Move)) {
+			throw new GameNodeException('node value is no instance of Move', 4);
+		}
+
+		$this->traitConstruct($move, $children);
+	}
+
+	/**
+ 	 * returns the first child, i.e. the main move
+ 	 *
+ 	 * @return GameNode the mainline move or false if there are no children
+ 	 */
+
+	public function getMainline()
+	{
+		$children = $this->getChildren();
+		return reset($children);
+	}
 }
--- tests/GameNodeTest.php
+++ tests/GameNodeTest.php
@@ -6,13 +6,42 @@ class GameNodeTest extends PHPUnit_Framework_TestCase
 {
 	public function testCreateGameNode()
 	{
-		$node = new GameNode();
+		$node = new GameNode(new Move('e2', 'e4'));
 		$this->assertInstanceOf('GameNode', $node);
 	}
 
 	public function testImplementsNodeInterface()
 	{
-		$node = new GameNode();
+		$node = new GameNode(new Move('e2', 'e4'));
 		$this->assertInstanceOf('Tree\Node\NodeInterface', $node);
+	}
+
+	public function testGetMainline()
+	{
+		$node = new GameNode(new Move('e2', 'e4'));
+		$node->addChild($node1 = new GameNode(new Move('e7', 'e5')));
+		$node->addChild($node2 = new GameNode(new Move('c7', 'c5')));
+		$this->assertEquals($node1, $node->getMainline());
+	}
+
+	public function testGetMainlineWithoutChildren()
+	{
+		$node = new GameNode(new Move('d2', 'd4'));
+		$this->assertFalse($node->getMainline());
+	}
+
+	/**
+ 	 * @expectedException     GameNodeException
+ 	 * @expectedExceptionCode 4
+ 	 */
+	public function testCreateInvalidNode()
+	{
+		$node = new GameNode('test');
+	}
+
+	public function testCreateNodeWithChildren()
+	{
+		$node = new GameNode(new Move('e2', 'e4'), [new GameNode(new Move('e7', 'e5'))]);
+		$this->assertSame(1, count($node->getChildren()));
 	}
 }
