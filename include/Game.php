634f689559ea1bc7e7c6ca10adde873bab56448f
jupiter24 <jupiter24@users.noreply.github.com>
2015-10-18 13:19:02 +0200

    Add docblocks and comments
    Game and GameNode had neither docblocks nor comments. That has been
    changed. There still aren't a lot of comments but I think most of it is
    pretty self-explanatory.

--- include/Game.php
+++ include/Game.php
@@ -1,5 +1,29 @@
 <?php
+/**
+ * This file contains the class Game
+ *
+ * This file includes Position.php, Move.php and GameNode.php
+ */
 
+/**
+ * Include necessary files
+ */
+require_once 'Position.php';
+
+/**
+ * Include necessary files
+ */
+require_once 'Move.php';
+
+/**
+ * Include necessary files
+ */
+require_once 'GameNode.php';
+
+/**
+ * A class representing a game of chess with variations.
+ *
+ */
 class Game
 {
 	public function __construct(Position $startingPosition = null)
@@ -11,12 +35,21 @@ class Game
 		$this->children = [];
 	}
 
+	/**
+	 * add a move at the current position of the game
+	 *
+	 * If there already is a move, a new variation will be added
+	 *
+	 * @param Move $move the move to add
+	 * @return void
+	 */
 	public function doMove(Move $move)
 	{
+		// check if move is legal
 		if (!$this->getPosition()->isLegalMove($move)) {
 			throw new Exception('Trying to add illegal Move');
 		}
-		if (empty($this->current)) {
+		if (empty($this->current)) { // pointer at starting position
 			$this->current = new GameNode($move);
 			$this->children[] = $this->current;
 			return;
@@ -24,37 +57,67 @@ class Game
 		$this->current = $this->current->addMove($move);
 	}
 
+	/**
+	 * go back by one move
+	 *
+	 * This method does not delete any moves! It just sets an internal pointer back by one move.
+	 *
+	 * @return void
+	 */
 	public function back()
 	{
-		if ((!$this->current->isChild()) || empty($this->current)) {
-			$this->current = null;
+		if ((!$this->current->isChild()) || empty($this->current)) { // current points to first move or starting position
+			$this->current = null; // set current to starting position
 			return;
 		}
 
 		$this->current = $this->current->getParent();
 	}
 
+	/**
+	 * get the current position
+	 *
+	 * This method does not return the position at the end of the mainline!
+	 *
+	 * @return Position The current position
+	 *
+	 */
 	public function getPosition()
 	{
-		if (empty($this->current)) {
+		if (empty($this->current)) { // current at starting position
 			return $this->startingPosition;
 		}
 		return $this->current->positionAfter($this->startingPosition);
 	}
 
+	/**
+	 * add a new variation of the last move that was played
+	 *
+	 * This is simply a combination of back() and doMove()
+	 *
+	 * @param Move $move The move to add as a variation
+	 * @return void
+	 */
 	public function addVariation(Move $move)
 	{
 		$this->back();
 		$this->doMove($move);
 	}
 
+	/**
+	 * jump back to the position after the last call of startVariation()
+	 *
+	 * Actually it doesn't matter whether you added a variation with startVariation() or with back() and doMove()
+	 *
+	 * @return void
+	 */
 	public function endVariation()
 	{
-		while ($this->current->isMainlineContinuation()) {
+		while ($this->current->isMainlineContinuation()) { // go back until beginning of variation
 			$this->back();
 		}
 
-		if (!$this->current->isChild()) {
+		if (!$this->current->isChild()) { // current at first move
 			$this->current = reset($this->children)->getMainlineContinuation();
 			return;
 		}
--- include/GameNode.php
+++ include/GameNode.php
@@ -1,7 +1,32 @@
 <?php
 
+/**
+ * This file contains the class GameNode and the corresponding exception class.
+ *
+ * This file includes Position.php and Move.php.
+ */
+
+/**
+ * Include necessary files
+ */
+require_once 'Position.php';
+
+/**
+ * Include necessary files
+ */
+require_once 'Move.php';
+
+/**
+ * A class representing an exception thrown by GameNode
+ */
 class GameNodeException extends Exception {}
 
+/**
+ * A class representing a move in a game
+ *
+ * This class is used by Game. In most cases you can simply use that class and don't really need to care how this one works.
+ *
+ */
 class GameNode
 {
 	public function __construct(Move $move, GameNode $parent = null)
@@ -13,52 +38,106 @@ class GameNode
 		$this->children = [];
 	}
 
+	/**
+	 * get the position after the move
+	 *
+	 * This method traverses the whole tree back to its root to calculate the position
+	 *
+	 * @param Position $startingPosition The position the root node starts with
+	 * @return Position The position after the move
+	 */
 	public function positionAfter(Position $startingPosition)
 	{
-		if (!$this->isChild()) {
+		if (!$this->isChild()) { // node has no parent
 			$pos = clone $startingPosition;
 			return $pos->doMove($this->move);
 		}
 		return $this->parent->positionAfter($startingPosition)->doMove($this->move);
 	}
 
+	/**
+	 * add another GameNode as a child to this one
+	 *
+	 * Be careful! This method does not set the parent attribute of the added node! It is recommended to use attachCild() instead.
+	 *
+	 * @param GameNode $child The GameNode to add
+	 * @return void
+	 */
 	public function addChild(GameNode $child)
 	{
 		$this->children[] = $child;
 	}
 
+	/**
+	 * add another GameNode as a child an set its parent attribute
+	 *
+	 * @param GameNode $child The GameNode to add
+	 * @return GameNode The child added
+	 */
 	public function attachChild(GameNode $child)
 	{
 		$child->attachTo($this);
 		return $child;
 	}
 
+	/**
+	 * add the GameNode as a child to another one
+	 *
+	 * @param GameNode $parent
+	 * @return void
+	 */
 	public function attachTo(GameNode $parent)
 	{
 		$this->parent = $parent;
 		$this->parent->addChild($this);
 	}
 
+	/**
+	 * create a new GameNode and attach it to this one
+	 *
+	 * @param Move $move
+	 * @return GameNode The new node
+	 */
 	public function addMove(Move $move)
 	{
 		return $this->attachChild(new GameNode($move));
 	}
 
+	/**
+	 * get the parent of the node
+	 *
+	 * @return GameNode the parent node
+	 */
 	public function getParent()
 	{
 		return $this->parent;
 	}
 
+	/**
+	 * return whether the node has a parent node or not
+	 *
+	 * @return bool whether the node is a child or not
+	 */
 	public function isChild()
 	{
 		return !empty($this->parent);
 	}
 
+	/**
+	 * get the first child of the node
+	 *
+	 * @return mixed The mainline continuation or false if the node has no children
+	 */
 	public function getMainlineContinuation()
 	{
 		return reset($this->children);
 	}
 
+	/**
+	 * return whether the node is the mainline continuation of its parent node
+	 *
+	 * @return bool Also returns true if the node has no parents!
+	 */
 	public function isMainlineContinuation()
 	{
 		if (!$this->isChild()) {
