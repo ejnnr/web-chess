a2f22d5e78f3d0a7a2865203c54d9490eef002ab
jupiter24 <jupiter24@users.noreply.github.com>
2015-12-22 12:23:09 +0100

    Upgrade to laravel 5.2
    This also means that 404 is now returned if a non-existing resource is
    accessed.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,11 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller extends BaseController {
 
-	use AuthorizesRequests, DispatchesCommands, ValidatesRequests;
+	use AuthorizesRequests, ValidatesRequests;
 
 }
