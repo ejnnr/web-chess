b063554305c894bf1a07d15e2b14f7889651658e
jupiter24 <jupiter24@users.noreply.github.com>
2016-02-10 13:02:55 +0100

    Automatically select new tab
    When opening a new tab, it is now automatically selected.
    
    TODO: When closing a tab, the last tab which was selected before should
    be selected.

--- resources/assets/ts/components/tabs/tabs.ts
+++ resources/assets/ts/components/tabs/tabs.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, ViewChild} from 'angular2/core';
 import {TabContentComponent} from '../tab-content/tab-content.component';
 import {Tab} from '../../interfaces/tab';
 import {TabListService} from '../../services/tab-list.service';
@@ -13,9 +13,19 @@ import {TabListService} from '../../services/tab-list.service';
 })
 export class TabsComponent
 {
+    @ViewChild('paperTabs')
+    paperTabs;
+
+    @ViewChild('ironPages')
+    ironPages;
+
     private _counter = 0;
 
     constructor(private _tabListService: TabListService) {}
+
+    selectTab(id: number) {
+        this.paperTabs.nativeElement.select(id);
+    }
 
     closeTab(tab: Tab) {
         this._tabListService.removeTab(tab);
@@ -26,9 +36,10 @@ export class TabsComponent
     }
 
     newTab() {
-        this._tabListService.addTab({
+        var tab = this._tabListService.addTab({
             "name": "Tab " + ++this._counter,
             "layoutName": "game"
         });
+        this.selectTab(tab.id);
     }
 }
--- resources/assets/ts/services/tab-list.service.ts
+++ resources/assets/ts/services/tab-list.service.ts
@@ -6,13 +6,16 @@ export class TabListService
 {
     private _tabs: Tab[] = [];
 
+    private _counter = 0;
+
     getTabs(): Tab[] {
         return this._tabs;
     }
 
-    addTab(tab: Tab) {
-        tab.id = this._tabs.length; // overwrite id to be the index of the tab in the array
+    addTab(tab: Tab): Tab {
+        tab.id = this._counter++; // overwrite id to be the index of the tab in the array
         this._tabs.push(tab);
+        return tab;
     }
 
     removeTab(tab: Tab) {
